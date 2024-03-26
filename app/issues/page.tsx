1db6eaa1b0e6d56723a645645ec39a0f76052e35
Anshik <guptaanshik1@gmail.com>
2024-03-27 00:19:08 +0530

    simple md integration done on create issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import React from "react";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const IssuesPage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Enter Title...." />
-      <TextArea placeholder="Enter Description...." />
+      <SimpleMDE placeholder="Enter Description...." />
       <Button>Submit New Issue</Button>
     </div>
   );
