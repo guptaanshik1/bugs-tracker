17962f624bd50287cb1e42e205ad7a8fd980ce79
Anshik <guptaanshik1@gmail.com>
2024-03-27 01:00:20 +0530

    moved issue form on a new route

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,43 +1,12 @@
-"use client";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
-import { Button, TextField } from "@radix-ui/themes";
-import { useForm, Controller } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import { ICreateIssueForm } from "@/utils/client/data/CreateIssue";
-import { createIssueFieldsConstants } from "@/utils/client/constants/formFieldsConstants";
-import axios from "axios";
-import { useRouter } from "next/navigation";
 
 const IssuesPage = () => {
-  const router = useRouter();
-  const { register, control, handleSubmit } = useForm<ICreateIssueForm>();
-
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        try {
-          await axios.post("/api/issues", data);
-          router.push("/issues");
-        } catch (err) {
-          console.error("Error while posting an issue:", err);
-        }
-      })}
-    >
-      <TextField.Root
-        placeholder="Enter Title...."
-        {...register(createIssueFieldsConstants.TITLE as "title")}
-      />
-      <Controller
-        name={createIssueFieldsConstants.DESCRIPTION as "description"}
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Enter Description...." {...field} />
-        )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+    <Button>
+      <Link href={"/issues/new"}>Add New Issue</Link>
+    </Button>
   );
 };
 
