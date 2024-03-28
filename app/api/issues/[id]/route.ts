4cc9249d2f50aafe49fc71a43cb9898b40f27c01
Anshik <guptaanshik1@gmail.com>
2024-03-28 15:27:06 +0530

    created api to update issue

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createIssueSchema } from "../schema";
+import prisma from "@/prisma/client";
+import { getPostResponse } from "@/utils/server/getResponseObject";
+
+interface IParams {
+  params: { id: string };
+}
+
+export const PUT = async (
+  request: NextRequest,
+  { params: { id } }: IParams
+) => {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+
+  if (!validation.success)
+    return getPostResponse(validation.error?.format(), 400);
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: +id,
+    },
+  });
+
+  if (!issue) return getPostResponse("Issue not found", 404);
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: +id },
+    data: { title: body.title, description: body.description },
+  });
+
+  return getPostResponse(updatedIssue, 200);
+};
