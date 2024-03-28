8063a44042d9af203fa061cffa435551ef925e61
Anshik <guptaanshik1@gmail.com>
2024-03-28 18:26:40 +0530

    added different modal for error if error occurs while deleting

--- app/issues/[id]/DeleteIssue.tsx
+++ app/issues/[id]/DeleteIssue.tsx
@@ -29,33 +29,56 @@ const DeleteIssue = ({ issueId }: IProps) => {
   };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button disabled={isLoading} color="red" onClick={handleIssueDelete}>
-          <AiTwotoneDelete />
-          Delete Issue
-          {isLoading && <Spinner />}
-        </Button>
-      </AlertDialog.Trigger>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button disabled={isLoading} color="red">
+            <AiTwotoneDelete />
+            Delete Issue
+            {isLoading && <Spinner />}
+          </Button>
+        </AlertDialog.Trigger>
 
-      <AlertDialog.Content>
-        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue changes cannot be reverted
-          once done?
-        </AlertDialog.Description>
-        <Flex gap="3" mt="4">
+        <AlertDialog.Content>
+          <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue changes cannot be
+            reverted once done?
+          </AlertDialog.Description>
+          <Flex gap="3" mt="4">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={handleIssueDelete}>
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            Some error occured while deleting the issue.
+          </AlertDialog.Description>
           <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
+            <Button
+              mt="2"
+              variant="soft"
+              color="gray"
+              onClick={() => setError(false)}
+            >
+              OK
             </Button>
           </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button color="red">Delete</Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
