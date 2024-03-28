07d8cf641b7e91256693f34a032c3526e6537159
Anshik <guptaanshik1@gmail.com>
2024-03-29 01:01:38 +0530

    added skeleton for text sign in

--- app/components/AuthStatus.tsx
+++ app/components/AuthStatus.tsx
@@ -4,10 +4,12 @@ import { Avatar, Box, DropdownMenu, Text } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const AuthStatus = () => {
   const { status, data: session } = useSession();
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
 
   if (status === "unauthenticated")
     return (
